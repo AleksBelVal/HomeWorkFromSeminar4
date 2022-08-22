328e87b5f932ed7d32266a0bf9489607b995ce94
Aleksandr <belyakov71@yandex.ru>
2022-08-22 21:10:37 +0300

    Домашняя работа. Задача №29

---
+++ HomeWork_Task29/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 29: Программа задаёт массив из 8 элементов 
+// выводит массив на экран.
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int[] arr = CreateArrayRndInt(8, -9, 9);
+PrintArray(arr);
